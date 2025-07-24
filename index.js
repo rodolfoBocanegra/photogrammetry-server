698ebead548084b1d9247359ccc7c43e9bb1944c
Rodolfo Bocanegra <rodobg96@gmail.com>
2025-07-24 23:14:09 +0000

    Refactor server listening log message for consistency

--- index.js
+++ index.js
@@ -160,5 +160,5 @@ app.post('/process', async (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+    console.log(`Server listening at port: ${port}`);
 });
